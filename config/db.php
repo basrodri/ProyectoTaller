9d08ce175ce588b730dbf0d9803b9162abf61c37
basrodri <basrodri@alumnos.ubiobio.cl>
2017-07-13 13:17:24 -0400

    Commit

---
+++ config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=taller',
+    'username' => 'root',
+    'password' => 'a',
+    'charset' => 'utf8',
+];
