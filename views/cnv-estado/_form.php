9bd95c438b1a5057e61eff3ca5fc51cf891d5b1d
remunoz <ricardoemh95@gmail.com>
2017-07-14 23:17:13 -0400

    Sacando widgetspor conflicto

--- views/cnv-estado/_form.php
+++ views/cnv-estado/_form.php
@@ -18,12 +18,10 @@ use dosamigos\ckeditor\CKEditor;
 
     <?= $form->field($model, 'NOMBRE_ESTADO_CONVENIO')->textInput(['maxlength' => true]) ?>
 
-    <?= $form->field($model, 'DESCRIPCION')->widget(CKEditor::className(), [
-   			 'preset' => 'basic'
-			]) ?>
-			
 
-    <?= $form->field($model, 'VIGENTE')->dropDownList(['S'=>'S','N'=>'N']) ?>
+    <?= $form->field($model, 'DESCRIPCION')->textArea(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'VIGENTE')->dropDownList(['s'=>'S','n'=>'N']) ?>
 
     <div class="form-group">
         <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
